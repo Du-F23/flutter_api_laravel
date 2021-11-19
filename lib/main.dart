6dd5e9fbacf54373ad9c25e3fb2b184dac267df7
2021 DSM43 Duarte V Fernando <al222010329@gmail.com>
2021-11-19 11:16:43 -0600

    Hicimos funcionar la api con flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,20 +22,23 @@ class MyApp extends StatelessWidget {
           child: Container(
               child: ElevatedButton(
             child: const Text('Clic aqui'),
-            onPressed: () async{
-              var url =
-      Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+            onPressed: () async {
+              var url = Uri.http('192.168.1.70:8000', '/api/productos', {});
 
-  // Await the http get response, then decode the json-formatted response.
-  var response = await http.get(url);
-  if (response.statusCode == 200) {
-    var jsonResponse =
-        convert.jsonDecode(response.body) as Map<String, dynamic>;
-    var itemCount = jsonResponse['totalItems'];
-    print('Number of books about http: $itemCount.');
-  } else {
-    print('Request failed with status: ${response.statusCode}.');
-  }
+              // Await the http get response, then decode the json-formatted response.
+              var response = await http.get(url);
+              if (response.statusCode == 200) {
+                var jsonResponse = convert
+                    .jsonDecode(response.body); // as Map<String, dynamic>;
+                //var itemCount = jsonResponse['totalItems'];
+                // ignore: unused_local_variable
+                String producto = jsonResponse[0]['producto'];
+                print('Producto 1: $producto');
+
+                //print(response.body);
+              } else {
+                print('Request failed with status: ${response.statusCode}.');
+              }
             },
           )),
         ),
