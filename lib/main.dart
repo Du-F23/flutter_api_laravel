a69a7a989d9adedb7b697f4cb04353f2df6bc9b1
2021 DSM43 Duarte V Fernando <al222010329@gmail.com>
2021-11-19 10:27:33 -0600

    Añadimos la paqueteria de http

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -16,9 +18,20 @@ class MyApp extends StatelessWidget {
           child: Container(
               child: ElevatedButton(
             child: const Text('Clic aqui'),
-            onPressed: () => {
-              // ignore: avoid_print
-              print('Boton pulsado')
+            onPressed: () async{
+              var url =
+      Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+
+  // Await the http get response, then decode the json-formatted response.
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonResponse =
+        convert.jsonDecode(response.body) as Map<String, dynamic>;
+    var itemCount = jsonResponse['totalItems'];
+    print('Number of books about http: $itemCount.');
+  } else {
+    print('Request failed with status: ${response.statusCode}.');
+  }
             },
           )),
         ),
